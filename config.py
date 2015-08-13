70168b2dd8dbb8e961b21f46359fd6beb5d86fa0
Mike Carr <mcarr@mcarr-mac.local>
2015-08-13 13:21:08 -0700

    add config

---
+++ config.py
@@ -0,0 +1,13 @@
+ALLOWED_EXTENSIONS = set(['csv'])
+
+PORT = 5000
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+UPLOAD_FOLDER = 'uploads/'
+
+DOARAMA_API_KEY='your key here'
